1bf9d5c880973828cbe58a774ff978c92b375f79
Aaron Bini <bini.aaron@gmail.com>
2016-07-25 12:58:33 -0700

    promisified tests

--- test/dataModule-test.js
+++ test/dataModule-test.js
@@ -5,52 +5,74 @@ const dataStore = require('../lib/dataModule');
 
 const data2 = new dataStore();
 
-describe('data storage module', function() {
+describe('data storage module', () => {
 
-  it('gets all stored items', function (done) {
-    data2.getAll(function(movies) {
+  it('gets all stored items', (done) => {
+    data2.getAll(data2.movies).then((movies) => {
       assert.deepEqual('[{"title":"Ghost Busters","year":1984}]', movies);
       done();
     });
   });
 
-  it('gets one stored item', function (done) {
-    data2.getOne({title: 'Ghost Busters'},function(movie) {
+  it('gets one stored item', (done) => {
+    data2.getOne({title: 'Ghost Busters'}).then((movie) => {
       assert.deepEqual('[{"title":"Ghost Busters","year":1984}]', movie);
       done();
     });
-
   });
 
-  it('adds a new item', function (done) {
-    data2.addMovie({title: 'The Godfather', year: 1973}, function(message) {
-      assert.isOk(data2.movies.length === 2);
+  it('errors when the GET request yields no item', (done) => {
+    data2.movies.splice(0,1);
+    data2.getOne({title: 'Ghost Busters'}).catch((err) => {
+      assert.isOk(data2.movies.length === 0);
+      assert.deepEqual('Sorry that movie is not in the database.', err);
+      done();
+    });
+  });
+
+  it('adds a new item', (done) => {
+    data2.addMovie({title: 'The Godfather', year: 1973}).then((message) => {
+      assert.isOk(data2.movies.length === 1);
       assert.deepEqual('Movie successfully added.', message);
       done();
     });
-
   });
 
-  it('replaces an item with a new item', function (done) {
-    data2.changeMovie({title: 'The Godfather'}, {title: 'The Catfather', year: 2016}, function(message) {
-      assert.isOk(data2.movies.length === 2);
-      assert.deepEqual('Movie successfully updated.', message);
+  it('errors when user tries to add item already in database', (done) => {
+    data2.addMovie({title: 'The Godfather', year: 1973}).catch((err) => {
+      assert.deepEqual('That movie is already in the database.', err);
       done();
     });
-    // done();
   });
 
-  it('deletes an item', function (done) {
-    data2.deleteMovie({title: 'The Catfather'}, function(message) {
-      assert.isOk(data2.movies.length === 1);
+  it('replaces an item with a new item', (done) => {
+    data2.changeMovie({title: 'The Godfather'}, {title: 'The Catfather', year: 2016})
+      .then((message) => {
+        assert.isOk(data2.movies.length === 1);
+        assert.deepEqual('Movie successfully updated.', message);
+        done();
+      });
+  });
+
+  it('errors when the PUT request item to replace is not found', (done) => {
+    data2.changeMovie({title: 'The Godfather'}, {title: 'Raising Arizona', year: 1987}).catch((err) => {
+      assert.deepEqual('Movie not found.', err);
+      done();
+    });
+  });
+
+  it('deletes an item', (done) => {
+    data2.deleteMovie({title: 'The Catfather'}).then((message) => {
+      assert.isOk(data2.movies.length === 0);
       assert.deepEqual('Movie successfully deleted.', message);
       done();
     });
-    // done();
   });
 
-  // after(function (done) {
-  //   data2.movies = [{title: 'Ghost Busters', year: 1984}];
-  //   done();
-  // });
+  it('errors when the DELETE request item to be deleted is not found', (done) => {
+    data2.deleteMovie({title: 'The Catfather'}).catch((err) => {
+      assert.deepEqual('Movie not found.', err);
+      done();
+    });
+  });
 });
